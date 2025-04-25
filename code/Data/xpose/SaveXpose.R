4b77aded2da68216d8316fec94a30ac2ef960c11
MadsMunkNielsen <madnie19@student.aau.dk>
2025-04-25 15:03:18 +0200

    BVla

--- code/Data/xpose/SaveXpose.R
+++ code/Data/xpose/SaveXpose.R
@@ -1,79 +1,79 @@
-SaveXpose_DV_VS_IPRED <- function(filename, plotname, folder = "Data/XposePlots/Dependent_Variable_vs_Individual_Predictions/") {
+SaveXpose_DV_VS_IPRED <- function(filename, plotname, folder = "Data/XposePlots/DV_Ipred/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_RES_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/Residuals_vs_Dependent_Variable/") {
+SaveXpose_RES_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/Res_Idv/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_RES_VS_PRED <- function(filename, plotname, folder = "Data/XposePlots/Residuals_vs_Model_Prediction/") {
+SaveXpose_RES_VS_PRED <- function(filename, plotname, folder = "Data/XposePlots/Res_Pred/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_ABSVAL_RES_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/Absolute_Residuals_vs_Dependent_Variable/") {
+SaveXpose_ABSVAL_RES_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/AbsRes_Idv/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_ABSVAL_RES_VS_PRED <- function(filename, plotname, folder = "Data/XposePlots/Absolute_Residuals_vs_Model_Prediction/") {
+SaveXpose_ABSVAL_RES_VS_PRED <- function(filename, plotname, folder = "Data/XposePlots/AbsRes_Pred/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_DV_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/Dependent_Variable_vs_ID/") {
+SaveXpose_DV_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/DV_Idv/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_IPRED_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/Individual_Predictions_vs_ID/") {
+SaveXpose_IPRED_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/Ipred_Idv/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_PRED_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/Model_Predictions_vs_ID/") {
+SaveXpose_PRED_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/Pred_Idv/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_DV_PRED_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/Dependent_Variable_vs_Predictions/") {
+SaveXpose_DV_PRED_VS_IDV <- function(filename, plotname, folder = "Data/XposePlots/DV_Pred_Idv/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_PRM_DISTRIB <- function(filename, plotname, folder = "Data/XposePlots/Parameter_Distribution/") {
+SaveXpose_PRM_DIST <- function(filename, plotname, folder = "Data/XposePlots/Prm_Dist/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_ETA_DISTRIB <- function(filename, plotname, folder = "Data/XposePlots/Random_Effects_Distribution/") {
+SaveXpose_ETA_DIST <- function(filename, plotname, folder = "Data/XposePlots/Eta_Dist/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_COV_DISTRIB <- function(filename, plotname, folder = "Data/XposePlots/Covariance_Distribution/") {
+SaveXpose_COV_DIST <- function(filename, plotname, folder = "Data/XposePlots/Cov_Dist/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_RES_DISTRIB <- function(filename, plotname, folder = "Data/XposePlots/Residuals_Distribution/") {
+SaveXpose_RES_DIST <- function(filename, plotname, folder = "Data/XposePlots/Res_Dist/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_PRM_QQ <- function(filename, plotname, folder = "Data/XposePlots/QQ_Plot_for_Parameters/") {
+SaveXpose_PRM_QQ <- function(filename, plotname, folder = "Data/XposePlots/PRM_QQ/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_ETA_QQ <- function(filename, plotname, folder = "Data/XposePlots/QQ_Plot_for_Random_Effects/") {
+SaveXpose_ETA_QQ <- function(filename, plotname, folder = "Data/XposePlots/Eta_QQ/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
 
-SaveXpose_COV_QQ <- function(filename, plotname, folder = "Data/XposePlots/QQ_Plot_for_Covariance/") {
+SaveXpose_COV_QQ <- function(filename, plotname, folder = "Data/XposePlots/Cov_QQ/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
@@ -87,4 +87,3 @@ SaveXpose_VPC <- function(filename, plotname, folder = "Data/XposePlots/VPC/") {
     full_path <- paste0(folder, filename)
     ggsave(full_path, plot = plotname, width = 13, height = 6, dpi = "retina")
 }
-
