1f34042e0e8f62c2fd981e7c1ade9efbc9414049
Magnus-Bolhuis <magnusbevb@gmail.com>
2025-04-29 09:19:19 +0200

    changed order of SP in Parameter plots

--- code/Data/xpose/parameterplots.R
+++ code/Data/xpose/parameterplots.R
@@ -1,23 +1,23 @@
 library(tidyverse)
 library(stringr)
+library(cowplot)
 
 Savegg <- function(filename, plotname, folder = "Data/XposePlots/nobsv/"){
     full_path <- paste0(folder, filename)
-    ggsave(full_path, plot = plotname, width = 11, height = 6, dpi = "retina")
+    ggsave(full_path, plot = plotname, width = 11, height = 8, dpi = "retina")
 }
 
 # comparing parameters with the data generating parameters
 true_parameters <- data.frame(
-  parameter = c("tka", "tq", "tcl", "tvc", "tvp"), 
-  True_Value = c(0.0253, 0.304, 0.0348, 3.59, 4.1)  
-)
+  parameter = c("tka", "tq", "tcl", "tvc", "tvp"),
+  True_Value = c(0.0253, 0.304, 0.0348, 3.59, 4.1))
 
 # Set colors
-colors <- c("#005AD2","#2A918B", "#EEA7BF", "#CCC5BD")
+colors <- c("#005AD2", "#2A918B", "#EEA7BF", "#001965")
 
 
 
-  parameter_plot <- function(list_of_parFixed_tables, name_of_model) {
+parameter_plot <- function(list_of_parFixed_tables, name_of_model) {
   # Making useful dataset for plotting
   parameters_for_plots <- data.frame()
   
@@ -34,7 +34,7 @@ colors <- c("#005AD2","#2A918B", "#EEA7BF", "#CCC5BD")
         lower_ci = as.numeric(str_extract(`Back-transformed(95%CI)`, "(?<=\\().+?(?=,)")),  # Extract lower CI
         upper_ci = as.numeric(str_extract(`Back-transformed(95%CI)`, "(?<=, ).+?(?=\\))")), # Extract upper CI
         parameter = rownames(current_df),
-        Model = factor(c('10', '25', '50', '100')[i], levels = c("10","25","50","100"))
+        Model = factor(c('10', '25', '50', '100')[i], levels = c("100","50","25","10"))
       ) %>%  
       select(parameter, Model, estimate, lower_ci, upper_ci) %>%
       filter(row.names(current_df) != "prop.sd")  # Filter out sd row
@@ -49,7 +49,7 @@ colors <- c("#005AD2","#2A918B", "#EEA7BF", "#CCC5BD")
 
   # Preparing for plotting
   # Create a named vector for colors
-  color_mapping <- setNames(colors, levels(parameters_for_plots$Model))
+  color_mapping <- setNames(colors, c("10","25","50","100"))
   
   for (current_parameter in unique(parameters_for_plots$parameter)) {
     # Filter data for the current parameter
@@ -58,6 +58,7 @@ colors <- c("#005AD2","#2A918B", "#EEA7BF", "#CCC5BD")
     
     # Ensure that filtered_data is not empty
     if (nrow(filtered_data) > 0) {
+
       # Make a facet plot
       p <- ggplot(filtered_data, aes(x = estimate , y = Model, color = Model, shape = Model)) +
         geom_point(size = 4) +  # Points for estimates
@@ -68,25 +69,39 @@ colors <- c("#005AD2","#2A918B", "#EEA7BF", "#CCC5BD")
         scale_color_manual(values = color_mapping) +  # Custom colors
         scale_shape_manual(values = c(16, 17, 18, 15)) +  # Custom shapes for points
         labs(y = NULL,
-             x = paste("Estimate of", current_parameter)) +  # Axis labels
+             x = paste("Estimate of", current_parameter),
+             color = "Number of subjects",
+             shape = "Number of subjects") +  # Axis labels
         theme_minimal(base_size = 20) +  # Minimal theme with larger base font size
         theme(
           axis.title.y = element_blank(),  # Further ensure the y-axis title is blank
           axis.ticks.y = element_blank(),   # Remove y-axis ticks
           axis.text.y = element_blank(),     # Remove y-axis text
-          plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
-          plot.subtitle = element_text(hjust = 0.5, size = 14),
+          axis.title.x = element_text(size = 25),  # X-axis title size
+          axis.text.x = element_text(size = 25),    # X-axis tick labels size
           legend.position = "none",
+          #legend.key.size = unit(2.8, "cm"),
+          #legend.text = element_text(size = 38, family = "serif",),  # Increase legend text size
+          #legend.title = element_text(size = 45, family = "serif",),  # Increase legend title size
+          #legend.background = element_rect(fill = "#fdfdfd", linetype = "solid"),  # Background for the legend
+          #legend.background = element_rect(fill = "lightgray", alpha = 0.5),
           panel.grid.major = element_line(color = "lightgray"),
-          panel.grid.minor = element_blank(),
-          aspect.ratio = 1/1.3  # Adjusted aspect ratio suitable for vertical plots
-        )
+          panel.grid.minor = element_blank())
+          #aspect.ratio = 1/1.3  # Adjusted aspect ratio suitable for vertical plots
+        
       
-      Savegg(paste(name_of_model, current_parameter, "errorbar.pdf", sep = "_"), p, folder = paste("Data/parameterplots/", name_of_model, "/", sep = ""))
+      # Extract the legend
+      
+         Savegg(paste(name_of_model, current_parameter, "errorbar.pdf", sep = "_"), p, folder = paste("Data/parameterplots/", name_of_model, "/", sep = ""))
     }
   }
 }
 
+      # Included to extract the legend of one of the plots
+      #if (current_parameter == "tka") {
+      #legend <- get_legend(p + theme(legend.position = "right"))  # You could also specify sizes or other theme elements
+      #ggsave("Data/parameterplots/legend.pdf", plot = legend, width = 9, height = 6, dpi = "retina", units = "in", limitsize = FALSE)
+      #}
 
 
 # Defining list with the different kinds of models
